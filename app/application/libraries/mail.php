c0e10545ff99c2849bc1eae6ad9d56701248a215
Michael Hasselbring <mikelbring@gmail.com>
2012-04-13 09:39:24 -0500

    mail - formatting issues

--- app/application/libraries/mail.php
+++ app/application/libraries/mail.php
@@ -1,52 +1,52 @@
 <?php
 class Mail {
 
-   /**
-    * Define mail transport
-	 *
-    * @param  string													  $default
-    * @return Swift_MailTransport|Swift_SendmailTransport
-    */
+	/**
+	* Define mail transport
+	*
+	* @param  string													  $default
+	* @return Swift_MailTransport|Swift_SendmailTransport
+	*/
 	private static function transport($default = null)
 	{
 		require path('vendor') . 'Swift/lib/swift_required.php';
 
 		$options = Config::get('application.mail');
 
-      if(is_null($default))
-      {
-         $default = $options['transport'];
-      }
+		if(is_null($default))
+		{
+			$default = $options['transport'];
+		}
 
-      switch($default)
-      {
-         case 'sendmail':
-            $transport = Swift_SendmailTransport::newInstance($options['sendmail']['path'].' -bs');
+		switch($default)
+		{
+			case 'sendmail':
+				$transport = Swift_SendmailTransport::newInstance($options['sendmail']['path'].' -bs');
 
-            break;
-         case 'smtp':
-            $transport = Swift_SmtpTransport::newInstance($options['smtp']['server'], $options['smtp']['port'])
-                ->setUsername($options['smtp']['username'])
-                ->setPassword($options['smtp']['password']);
+				break;
+			case 'smtp':
+				$transport = Swift_SmtpTransport::newInstance($options['smtp']['server'], $options['smtp']['port'])
+					->setUsername($options['smtp']['username'])
+					->setPassword($options['smtp']['password']);
 
-            break;
-         default:
-            $transport = Swift_MailTransport::newInstance();
+				break;
+			default:
+				$transport = Swift_MailTransport::newInstance();
 
-            break;
-      }
+			break;
+		}
 
 		return $transport;
 	}
 
-   /**
-    * Send the requested message
-    *
-    * @param   string  $message
-    * @param   string  $to
-    * @param   string  $subject
-    * @return  int
-    */
+	/**
+	* Send the requested message
+	*
+	* @param   string  $message
+	* @param   string  $to
+	* @param   string  $subject
+	* @return  int
+	*/
 	public static function send_email($message, $to, $subject)
 	{
 		$options = Config::get('application.mail');
@@ -56,9 +56,9 @@ class Mail {
 		$mailer = Swift_Mailer::newInstance($transport);
 
 		$message = Swift_Message::newInstance($subject)
-		   ->setFrom(array($options['from']['email'] => $options['from']['name']))
-		   ->setTo(array($to))
-		   ->setBody($message,'text/html');
+			->setFrom(array($options['from']['email'] => $options['from']['name']))
+			->setTo(array($to))
+			->setBody($message,'text/html');
 
 		$result = $mailer->send($message);
 
