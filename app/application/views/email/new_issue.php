85b8a0e7c15f3899e0334f433444040945e3e4e3
Jakub Kadlčík <frostyx@email.cz>
2013-09-04 23:39:43 +0200

    Notify user when creating issue assigned to him

---
+++ app/application/views/email/new_issue.php
@@ -0,0 +1,8 @@
+<p>
+Hi, <?php echo $firstname ?><br />
+on <?php echo URL::base() ?> website has been created a new issue '<?php echo $issue->title ?>' which has been assigned to you.<br />
+<br />
+Hafe fun :-)
+</p>
+<br />
+<p>URL: <?php echo $issue->to() ?>
