51754e860118f2dc6a50fbdef9b60e822bbd1b08
Jakub Kadlčík <frostyx@email.cz>
2013-09-05 00:10:33 +0200

    Notify user commentingating issue assigned to him

---
+++ app/application/views/email/commented_issue.php
@@ -0,0 +1,8 @@
+<p>
+Hi, <?php echo $firstname ?><br />
+on <?php echo URL::base() ?> has been commented an issue '<?php echo $issue->title ?>' which is assigned to you.<br />
+<br />
+I hope it will help you in solving it.
+</p>
+<br />
+<p>URL: <?php echo $issue->to() ?>
