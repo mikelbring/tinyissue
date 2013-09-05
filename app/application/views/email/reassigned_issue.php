7ed5aa395ef27a2a9080e007e0b26a55488134c1
Jakub Kadlčík <frostyx@email.cz>
2013-09-05 06:48:28 +0200

    Notify user when issue is reassigned to him

---
+++ app/application/views/email/reassigned_issue.php
@@ -0,0 +1,8 @@
+<p>
+Hi, <?php echo $firstname ?><br />
+on <?php echo URL::base() ?> has been reassigned an issue '<?php echo $issue->title ?>' to you.<br />
+<br />
+I hope that you get lucky and solve it soon :-)
+</p>
+<br />
+<p>URL: <?php echo $issue->to() ?>
