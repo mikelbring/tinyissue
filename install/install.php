d735514339bafc1912728f594a2020351c1ad927
Alan Farquharson <farkie0390@gmail.com>
2013-09-09 17:36:40 +0100

    Used MySQLi instead of Mysql_ which has been deprecated

--- install/install.php
+++ install/install.php
@@ -2,32 +2,31 @@
 
 class install
 {
-	public $config;
+	protected $config;
+	protected $connect;
+	public $error;
 
 	function __construct()
 	{
 		$this->config = require '../config.app.php';
 		$this->mysql_structure = require './mysql-structure.php';
 
+		$this->check_connect();
 	}
 
 	public function check_connect()
 	{
-		@$connect = mysql_connect($this->config['database']['host'],$this->config['database']['username'],$this->config['database']['password']);
 
-		if(!$connect)
+		if(!$this->connect)
 		{
-			return array('error' => '<strong>Database Connect Error.</strong>!');
+			try {
+				$this->connect = mysqli_connect($this->config['database']['host'],$this->config['database']['username'],$this->config['database']['password'], $this->config['database']['database']);
+			} catch(Exception $e) {
+				echo $this->error = 'Could not connect to database';
+			}
 		}
 
-		$check_db = $this->check_db($connect);
-
-		if(!$check_db)
-		{
-			 return array('error' => '<strong>Database Error.</strong>');
-		}
-
-		return $check_db;
+		return $this;
 	}
 
 	public function check_requirements()
@@ -61,67 +60,42 @@ class install
 	{
 		foreach($this->mysql_structure as $query)
 		{
-			mysql_query($query);
+			$stmt = $this->connect->prepare($query);
+			$stmt->execute();
 		}
 
 		/* Create Administrator Account */
 		$role = 4;
-		$email = mysql_real_escape_string($_POST['email']);
-		$first_name = mysql_real_escape_string($_POST['first_name']);
-		$last_name = mysql_real_escape_string($_POST['last_name']);
+		$email = $_POST['email'];
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
 		$password = Laravel\Hash::make($_POST['password']);
 
 		/* Check if email exists if so change the password on it */
-		$test_query = "select * from users where email = '$email' and deleted = 0 LIMIT 1";
-		$test_result = mysql_query($test_query);
+		$stmt = $this->connect->prepare("SELECT * FROM users WHERE email = ? AND deleted = 0");
+		$stmt->bind_param("s", $email);
 
-		if(mysql_num_rows($test_result) >= 1)
+		$stmt->execute();
+		$stmt->store_result();
+
+		if($stmt->num_rows >= 1)
 		{
-			$query = "
-			UPDATE `users`
-			SET
-				password = '{$password}',
-				firstname = '{$first_name}',
-				lastname = '{$last_name}'
-			WHERE email = '{$email}' AND deleted = 0
-			LIMIT 1
-			";
+			$query = $this->connect->prepare("UPDATE `users` SET password = ?, firstname = ?, lastname = ? WHERE email = ? AND deleted = 0 LIMIT 1");
+			$query->bind_param('s', $password);
+			$query->bind_param('s', $first_name);
+			$query->bind_param('s', $last_name);
+			$query->bind_param('s', $email);
 		}
 		else
 		{
-			$query = "
-			INSERT INTO users(
-				role_id,
-				email,
-				password,
-				firstname,
-				lastname,
-				created_at
-			)VALUES(
-				'$role',
-				'$email',
-				'$password',
-				'$first_name',
-				'$last_name',
-			now()
-			)";
+			$query = $this->connect->prepare("INSERT INTO users(role_id, email, password, firstname, lastname, created_at) VALUES (?, ?, ?, ?, ?, NOW())");
+			$query->bind_param('issss', $role, $email, $password, $first_name, $last_name);
 
 		}
 
-		mysql_query($query);
+		$query->execute();
 
 		return true;
 	}
 
-	private function check_db($connect)
-	{
-		@$database_connect = mysql_select_db($this->config['database']['database'], $connect);
-
-		if($database_connect)
-		{
-			return $database_connect;
-		}
-
-		return false;
-	}
 }
