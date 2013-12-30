be710faffb1b469ef9defd22f45048228000ddda
Florent Denis <dflorent.pokap@gmail.com>
2013-12-30 14:08:02 +0000

    Fix issue views time class

--- app/views/user/issues.php
+++ app/views/user/issues.php
@@ -2,16 +2,13 @@
     <?php echo trans('tinyissue.your_issues'); ?>
     <span><?php echo trans('tinyissue.your_issues_description'); ?></span>
 </h3>
-
 <div class="pad">
-
     <?php foreach ($projects as $project): ?>
-
     <div class="blue-box">
         <div class="inside-pad">
             <h4><a href="<?php echo $project['detail']->to(); ?>"><?php echo $project['detail']->name; ?></a></h4>
             <ul class="issues">
-                <?php foreach($project['issues'] as $row):  ?>
+                <?php foreach ($project['issues'] as $row):  ?>
                 <li>
                     <a href="<?php echo $row->to(); ?>" class="comments"><?php echo $row->comment_count(); ?></a>
                     <a href="<?php echo $row->to(); ?>" class="id">#<?php echo $row->id; ?></a>
@@ -20,21 +17,18 @@
                         <div class="info">
                             <?php echo trans('tinyissue.created_by'); ?>
                             <strong><?php echo $row->user->firstname . ' ' . $row->user->lastname; ?></strong>
-                            <?php echo Time::age(strtotime($row->created_at)); ?>
+                            <?php echo Tinyissue\Views\Time::age(strtotime($row->created_at)); ?>
 
-                            <?php if(!is_null($row->updated_by)): ?>
+                            <?php if ( ! is_null($row->updated_by)): ?>
                             - <?php echo trans('tinyissue.updated_by'); ?> <strong><?php echo $row->updated->firstname . ' ' . $row->updated->lastname; ?></strong>
-                            <?php echo Time::age(strtotime($row->updated_at)); ?>
+                            <?php echo Tinyissue\Views\Time::age(strtotime($row->updated_at)); ?>
                             <?php endif; ?>
                         </div>
                     </div>
                 </li>
                 <?php endforeach; ?>
             </ul>
-
         </div>
     </div>
-
     <?php endforeach; ?>
-
 </div>
--- config.app.example.php
+++ config.app.example.php
@@ -2,17 +2,17 @@
 
 return array(
 
-	/**
-	 * You can define your URL or leave blank to let us figure it out
-	 * 	- Sometimes in non-apache setups you need to define your url
-	 */
-	'url' => '',
+    /**
+     * You can define your URL or leave blank to let us figure it out
+     *     - Sometimes in non-apache setups you need to define your url
+     */
+    'url' => '',
 
-	/**
-	 * Fill out your database settings. Make sure that the driver is correct: 'mysql' for MySQL, 'sqlsrv' for MSSQL,
-	 * 'pgsql' for PostgreSQL, or 'sqlite' for SQLite
-	 */
-	'database' => array(
+    /**
+     * Fill out your database settings. Make sure that the driver is correct: 'mysql' for MySQL, 'sqlsrv' for MSSQL,
+     * 'pgsql' for PostgreSQL, or 'sqlite' for SQLite
+     */
+    'database' => array(
         'driver'    => 'mysql',
         'host'      => 'localhost',
         'database'  => 'database_name',
@@ -21,51 +21,51 @@ return array(
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => '',
-	),
+    ),
 
-	/**
-	 * Mail Settings
-	 * - Put in the name and email you would like email from Tiny Issue to come from
-	 * - This is usually only for setting up new accounts
-	 *
-	 * Default Mail Transport
-	 *	|
-	 *	| Possible Values
-	 *	| 	mail (PHP Mail Function)
-	 *	| 	sendmail (Sendmail)
-	 *	| 	smtp (Define Custom SMTP)
-	 *
-	 */
-	'mail' => array(
+    /**
+     * Mail Settings
+     * - Put in the name and email you would like email from Tiny Issue to come from
+     * - This is usually only for setting up new accounts
+     *
+     * Default Mail Transport
+     *    |
+     *    | Possible Values
+     *    |     mail (PHP Mail Function)
+     *    |     sendmail (Sendmail)
+     *    |     smtp (Define Custom SMTP)
+     *
+     */
+    'mail' => array(
 
-		'from' => array(
-			'name' => 'Your E-Mail Name',
-			'address' => 'name@domain.com',
-		),
+        'from' => array(
+            'name' => 'Your E-Mail Name',
+            'address' => 'name@domain.com',
+        ),
 
-		/**
-		 * Transport Settings
-		 *
-		 * Transport settings if using sendmail or SMTP
-		 */
-		'sendmail' => '/usr/sbin/sendmail -bs',
+        /**
+         * Transport Settings
+         *
+         * Transport settings if using sendmail or SMTP
+         */
+        'sendmail' => '/usr/sbin/sendmail -bs',
 
         'host' => 'smtp.mailgun.org',
 
         'port' => 587,
 
-	),
+    ),
 
-	/**
-	 * Specify your timezone
-	 * - http://php.net/manual/en/timezones.php
-	 */
-	'timezone' => 'America/Chicago',
+    /**
+     * Specify your timezone
+     * - http://php.net/manual/en/timezones.php
+     */
+    'timezone' => 'America/Chicago',
 
-	/**
-	 * Put in a random key combination to use as your session keys
-	 * Up to 32 characters
-	 */
-	'key' => 'yourrandomkey',
+    /**
+     * Put in a random key combination to use as your session keys
+     * Up to 32 characters
+     */
+    'key' => 'yourrandomkey',
 
 );
