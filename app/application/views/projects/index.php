473ec5222d3ddadf2b0cf5996fea6586e0fa5a91
Steve <steve@irrational.ca>
2014-02-02 05:56:25 -0500

    Theming, add icon, remove sortable project experimental code

--- app/application/views/projects/index.php
+++ app/application/views/projects/index.php
@@ -23,11 +23,11 @@
 		<div class="blue-box">
 
 			<div class="inside-pad">
-				<ul class="projects" id="sortable">
+				<ul class="projects">
 					<?php foreach($projects as $row):
 						$issues = $row->issues()->where('status', '=', 1)->count();
 					?>
-					<li class="sortable-li" data-project-id="<?php echo $row->id; ?>">
+					<li>
 						<a href="<?php echo $row->to(); ?>"><?php echo $row->name; ?></a><br />
 						<?php echo $issues == 1 ? '1 '. __('tinyissue.open_issue') : $issues . ' '. __('tinyissue.open_issues'); ?>
 					</li>
@@ -39,77 +39,13 @@
 					</li>
 					<?php endif; ?>
 				</ul>
+				
 
 
 			</div>
 
 		</div>
-			
-		<div id="sortable-msg"></div>
-		<div id="sortable-save"><input id="sortable-save-button" class="button primary" type="submit" value="SAVE" /></div>
 
 	</div>
 
 </div>
-
-<style>
-.sortable-li,
-ul.projects li {
-	line-height: 1.2em;
-	height: 25px;
-}
-#sortable-save {
-	margin-top: 10px;
-}
-#sortable-msg {
-	color: #107AA7;
-	font-weight: bold;
-}
-#sortable-msg.error {
-	color: #F21D20;
-}
-</style>
- <script>
-$(function() {
-	$('#sortable-save').hide();
-	
-	// Apply sortable behaviours.
-	$( "#sortable" ).sortable({
-		placeholder: "sortable-li",
-		change: function( event, ui ) {
-			$('#sortable-save').show();
-			$('#sortable-msg').text("Your new ordering is not yet saved.")
-			$('#sortable-msg').removeClass('error');
-		}
-	});
-	$( "#sortable" ).disableSelection();
-	
-	// Save it.
-	$('#sortable-save-button').click(function(){
-		var weights = new Array();
-		$('ul.projects li').each(function (index) {
-			weights.push($(this).data('project-id'));
-		});
-		
-		var data = new Array();
-		data['target'] = 'projects';
-		data['sorted'] = weights;
-		
-		// Test
-		console.log(data);
-		$('#sortable-save').hide();
-		$('#sortable-msg').text("Changes saved");
-		
-    // POST
-    /*
-    $.post(siteurl + 'ajax/sortable/projects', {
-      weights : weights
-    }, function(data){
-			$('#sortable-save').hide();
-			$('#sortable-msg').text("Changes saved");
-		});
-    */
-    
-  });
-});
-</script>
