c832e55ab3d9fdfdb956b51c5e989e9c7e8de5e5
Cyril Bornet <cyril.bornet@gmail.com>
2014-07-09 10:30:07 +0200

    fixed faulty closing tag in topbar title

--- app/application/views/project/issue/activity/reassign-issue.php
+++ app/application/views/project/issue/activity/reassign-issue.php
@@ -1,12 +1,12 @@
 <li id="comment<?php echo $activity->id; ?>" class="comment">
 
 	<div class="insides">
-		<div class="topbar">		
+		<div class="topbar">
 			<label class="label warning"><?php echo __('tinyissue.label_reassigned'); ?></label> <?php echo __('tinyissue.to'); ?>
 			<?php if($activity->action_id > 0): ?>
 			<strong><?php echo $assigned->firstname . ' ' . $assigned->lastname; ?></strong>
 			<?php else: ?>
-			<strong><?php echo __('tinyissue.no_one'); ?>/strong>
+			<strong><?php echo __('tinyissue.no_one'); ?></strong>
 			<?php endif; ?>
 			by
 			<strong><?php echo $user->firstname . ' ' . $user->lastname; ?></strong>
