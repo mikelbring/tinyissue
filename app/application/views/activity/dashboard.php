570f0904bce8ab8a40cfbb9a9776fa78abd3b5d2
Patriboom <foi02@cartefoi.net>
2017-02-13 14:34:28 -0500

    Dashbord s items sorted alphabetically

--- app/application/views/activity/dashboard.php
+++ app/application/views/activity/dashboard.php
@@ -6,23 +6,40 @@
 </h3>
 
 <div class="pad">
-	<?php foreach(Auth::user()->dashboard() as $project):
+<?php 
+	foreach(Auth::user()->dashboard() as $project):
 		if(!$project['activity']) continue;
-	?>
+
+		$id = $project['project']->attributes['id'];
+		$NomProjet[$id] = $project['project']->name;
+		$SansAccent[$id] = htmlentities($NomProjet[$id], ENT_NOQUOTES, 'utf-8');
+		$SansAccent[$id] = preg_replace('#&([A-za-z])(?:uml|circ|tilde|acute|grave|cedil|ring);#', '\1', $SansAccent[$id]);
+		$SansAccent[$id] = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $SansAccent[$id]);
+		$SansAccent[$id] = preg_replace('#&[^;]+;#', '', $SansAccent[$id]);		
+
+		foreach($project['activity'] as $activity) {
+			$actiProj[$id][] =  $activity;
+		}
+		asort($SansAccent);
+	endforeach;
+
+	foreach ($SansAccent as $id => $name): 
+?>
+
 	<div class="blue-box">
 		<div class="inside-pad">
 
 			<h4>
-				<a href="<?php echo $project['project']->to(); ?>"><?php echo $project['project']->name; ?></a>
+				<a href="project/<?php echo $id; ?>"><?php echo $NomProjet[$id]; ?></a>
 			</h4>
 
 			<ul class="activity">
-				<?php foreach($project['activity'] as $activity): ?>
+				<?php foreach($actiProj[$id] as $activity): ?>
 				<?php echo $activity; ?>
 				<?php endforeach; ?>
 			</ul>
 
-			<a href="<?php echo $project['project']->to(); ?>" class="view"><?php echo $project['project']->name; ?></a>
+			<a href="project/<?php echo $id; ?>"><?php echo $NomProjet[$id]; ?></a>
 
 		</div>
 	</div>
