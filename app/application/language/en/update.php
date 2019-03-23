4a8d8d69c09561a6400f64cab87a14a250be7f09
Patrick Allaire, ptre <foi02@cartefoi.net>
2019-03-23 10:38:40 -0400

    Les mises à jour tiendront dorénavant compte des modifications à la page config.app.php

--- app/application/language/en/update.php
+++ app/application/language/en/update.php
@@ -1,5 +1,9 @@
 <?php
 return array(
+	'Description_1' => 'Program`s files update',
+	'Description_2' => 'Database update',
+	'Description_3' => 'Preferences update',
+	'Description_4' => 'Your application has been updated',
 	'Intro_0' => 'Some updates are available.  Please proceed update',
 	'Intro_1' => 'Two ways to update your « Bugs »',
 	'Intro_2' => 'Download and unzip the following file : ',
@@ -10,5 +14,9 @@ return array(
 	'updateData_1' => 'The updated programm is ready to use.',
 	'updateData_2' => 'You must approve every change among the following ones (security concern)',
 	'updateData_3' => 'Apply',
-	'ResultData_1' => 'Update is completed.'
+	'updateData_4' => 'There no update to apply on your database.',
+	'updateData_5' => 'There`s no new preferences definition in the new version.',
+	'ResultData_1' => 'Your database is updated.',
+	'ResultData_3a' => 'Your preferences file is updated.',
+	'ResultData_3b' => 'For your conveniance, a copy of your original file has been saved under '
 );
