489543ac91b6ee0476b9d4e3601529738d8af0a0
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-04-21 19:50:11 -0400

    Working email options

--- app/application/controllers/ajax/ChgConfEmail.php
+++ app/application/controllers/ajax/ChgConfEmail.php
@@ -1,47 +1,51 @@
 <?php
-$prefixe = "";
-while (!file_exists($prefixe."config.app.php")) {
-	$prefixe .= "../";
-}
+	$prefixe = "";
+	while (!file_exists($prefixe."config.app.php")) {
+		$prefixe .= "../";
+	}
 	//Définition des variables
 	$MesLignes = array();
 	$NumLigne = array();
 	$NomFichier = $prefixe."config.app.php";
+	$rendu = 0;
 
 	//Sauvegarde du fichier original
 	$SavFichier = "config.app.".date("Ymd").".php";
 	copy ($prefixe."config.app.php", $prefixe.$SavFichier);
-	
 
-	Lecture du fichier de configuration
+	//Lecture du fichier de configuration
 	////Ouvrons le fichier de configuration
-	$MonFichier = fopen($prefixe."config.app.php","r");
+	$RefFichier = fopen($NomFichier, "r");
 	////Boucle de lecture
-	while (!feof($DefFichier)) {
-		$MesLignes[] = fgets($MonFichier);
+	while (!feof($RefFichier)) {
+		$MesLignes[$rendu] = fgets($RefFichier);
+		if (strpos($MesLignes[$rendu], "mail' => array") !== false && !isset($NumLigne["mail"])) { $NumLigne["mail"] = $rendu; }  
+		if (strpos($MesLignes[$rendu], "* Final delivery format") !== false) { $NumLigne["forma"] = $rendu; }  
+		++$rendu;
 	}
-	fclose($MonFichier);
+	fclose($RefFichier);
 
 	//Repérage des infomations intérssantes
-	$NumLigne["mail"] = array_search("	'mail' => array(", $MesLignes);
 	$NumLigne["fName"] = $NumLigne["mail"] + 2; 
 	$NumLigne["fMail"] = $NumLigne["mail"] + 3; 
 	$NumLigne["rName"] = $NumLigne["mail"] + 6; 
 	$NumLigne["rMail"] = $NumLigne["mail"] + 7; 
-	$NumLigne["mail"] = array_search("	* Final delivery format", $MesLignes);
-	$NumLigne["intro"] = $NumLigne["mail"] + 6; 
-	$NumLigne["bye"] = $NumLigne["mail"] + 7; 
+	$NumLigne["intro"] = $NumLigne["forma"] + 6; 
+	$NumLigne["bye"]   = $NumLigne["forma"] + 7;
+	unset( $NumLigne["mail"], $NumLigne["forma"]); 
 
 	//Changement des lignes repérées à la faveur des données soumises par l'usager
-	$MesLignes[$NumLigne["fName"]] =  $_GET["fName"]; 
-	$MesLignes[$NumLigne["fMail"]] =  $_GET["fMail"]; 
-	$MesLignes[$NumLigne["rName"]] =  $_GET["rName"]; 
-	$MesLignes[$NumLigne["rMail"]] =  $_GET["rMail"]; 
-	$MesLignes[$NumLigne["intro"]] =  $_GET["intro"]; 
-	$MesLignes[$NumLigne["bye"]]   =  $_GET["bye"]; 
+	$posi = 0;
+	foreach ($NumLigne as $ind => $lgn) {
+		$MesLignes[$NumLigne[$ind]] = substr($MesLignes[$NumLigne[$ind]], 0, strpos($MesLignes[$NumLigne[$ind]], "=>") + 3 )."'".str_replace("'", "`",$_GET[$ind])."',
+"; 
+	}
 	
 	//Enregistrement du nouveau fichier corrigé  
 	$NeoFichier = fopen($NomFichier, "w");
-	fwrite($NeoFichier, $NeoContenu);
+	foreach ($MesLignes as $ind => $val) {
+		fwrite($NeoFichier, $val);
+	}
 	fclose($NeoFichier);
+	echo 'Nous avons terminé';
 ?>
