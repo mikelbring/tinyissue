e298c32b069aa558cfb8217fb4321cf1049a275e
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-04-21 00:47:34 -0400

    Deuxième version, basée sur un tableau en mémoire .... reste à faire le enregistrement du fichier

--- app/application/controllers/ajax/ChgConfEmail.php
+++ app/application/controllers/ajax/ChgConfEmail.php
@@ -3,72 +3,45 @@ $prefixe = "";
 while (!file_exists($prefixe."config.app.php")) {
 	$prefixe .= "../";
 }
-echo 'Je suis au début - ligne 6';
-			//Sauvegarde du fichier original
-			$SavFichier = "config.app.".date("Ymd").".php";
-			copy ($prefixe."config.app.php", $prefixe.$SavFichier);
-			
-			//Colligeons le rang des clefs
-			////Clefs de préférences activées par l'usager
-			$MesKeys = array_keys($MesPref);
-			////Clefs du fichier de configurations par défaut
-			$DefKeys = array_keys($DefPref);
-echo 'Je suis au début - ligne 16';
+	//Définition des variables
+	$MesLignes = array();
+	$NumLigne = array();
+	$NomFichier = $prefixe."config.app.php";
 
-			//Ouvrons le fichier de configuration
-			$NeoContenu = "";
-			$NomFichier = $prefixe."config.app.php";
-			////Fichier de préférences de l'usager
-			$MonFichier = fopen($prefixe."config.app.php","r");
-echo 'Voici la ligne 23';
+	//Sauvegarde du fichier original
+	$SavFichier = "config.app.".date("Ymd").".php";
+	copy ($prefixe."config.app.php", $prefixe.$SavFichier);
+	
 
-			////Boucle de lecture
-			while (!feof($DefFichier)) {
-				$MonLigne = fgets($MonFichier);
-				$NeoContenu .= $MonLigne;
-				if ($MonLigne == "'mail' => array(") { break; } 
-			}
-			//Avons repéré la section du courriel
-			////Sautons deux lignes afin de redéfinir la valeur du courriel FROM
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= $MonLigne;
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= $MonLigne;
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= "'name' => '".$_GET["fName"]."',";
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= "'name' => '".$_GET["fMail"]."',";
-			////Deux autres lignes puis l'adresse ReplyTo
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= $MonLigne;
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= $MonLigne;
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= "'name' => '".$_GET["rName"]."',";
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= "'name' => '".$_GET["rMail"]."',";
-			////Reprise de la lecture avant de modifier intro et bye
-			while (!feof($DefFichier)) {
-				$MonLigne = fgets($MonFichier);
-				if (substr(trim($MonLigne), 0, 10) == "'intro' =>") { break; } 
-				$NeoContenu .= $MonLigne;
-			}
-			////Mise à jour de la valeur intro
-			$NeoContenu .= "'intro' => '".$_GET["intro"]."', ";
-			////Mise à jour de la valeur bye
-			$MonLigne = fgets($MonFichier);
-			$NeoContenu .= "'bye' => '".$_GET["bye"]."', ";
-			
-			while (!feof($DefFichier)) {
-				$MonLigne = fgets($MonFichier);
-				$NeoContenu .= $MonLigne;
-			}
-			fclose($MonFichier);
+	Lecture du fichier de configuration
+	////Ouvrons le fichier de configuration
+	$MonFichier = fopen($prefixe."config.app.php","r");
+	////Boucle de lecture
+	while (!feof($DefFichier)) {
+		$MesLignes[] = fgets($MonFichier);
+	}
+	fclose($MonFichier);
 
-			//Conservons le nouveau fichier de préférences  
-			$NeoFichier = fopen($NomFichier, "w");
-			fwrite($NeoFichier, $NeoContenu);
-			fclose($NeoFichier);
-			echo 'Voici le nouveau fichier';
-			echo $NeoContenu;
+	//Repérage des infomations intérssantes
+	$NumLigne["mail"] = array_search("	'mail' => array(", $MesLignes);
+	$NumLigne["fName"] = $NumLigne["mail"] + 2; 
+	$NumLigne["fMail"] = $NumLigne["mail"] + 3; 
+	$NumLigne["rName"] = $NumLigne["mail"] + 6; 
+	$NumLigne["rMail"] = $NumLigne["mail"] + 7; 
+	$NumLigne["mail"] = array_search("	* Final delivery format", $MesLignes);
+	$NumLigne["intro"] = $NumLigne["mail"] + 6; 
+	$NumLigne["bye"] = $NumLigne["mail"] + 7; 
+
+	//Changement des lignes repérées à la faveur des données soumises par l'usager
+	$MesLignes[$NumLigne["fName"]] =  $_GET["fName"]; 
+	$MesLignes[$NumLigne["fMail"]] =  $_GET["fMail"]; 
+	$MesLignes[$NumLigne["rName"]] =  $_GET["rName"]; 
+	$MesLignes[$NumLigne["rMail"]] =  $_GET["rMail"]; 
+	$MesLignes[$NumLigne["intro"]] =  $_GET["intro"]; 
+	$MesLignes[$NumLigne["bye"]]   =  $_GET["bye"]; 
+	
+	//Enregistrement du nouveau fichier corrigé  
+	$NeoFichier = fopen($NomFichier, "w");
+	fwrite($NeoFichier, $NeoContenu);
+	fclose($NeoFichier);
 ?>
