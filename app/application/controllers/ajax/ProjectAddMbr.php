e9d3b743e493e11bd7b05bf5eeb97e30add20901
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-04-24 13:02:21 -0400

    New user into project automaticaly is a new follower of this project

--- app/application/controllers/ajax/ProjectAddMbr.php
+++ app/application/controllers/ajax/ProjectAddMbr.php
@@ -4,6 +4,7 @@
 	$_GET["User"] = strtolower($_GET["User"]);
 	$requUSER  = "INSERT INTO projects_users VALUES (NULL, ".$_GET["User"].", ".$_GET["Projet"].", 2, NOW(), NOW()) ";
 	if (Requis($requUSER)) {
+		Requis("INSERT INTO following VALUES (NULL, ".$_GET["User"].", ".$_GET["Projet"].", 0, 1, 1, 1) ");
 		$QuelPERS = Explose("SELECT firstname, UPPER(lastname) AS lastname FROM users WHERE id = ".$_GET["User"]);
 		//$membres .= '<li id="project-user'.$_GET["User"].'">';
 		$membres .= $QuelPERS["firstname"] . ' ' . $QuelPERS["lastname"];
