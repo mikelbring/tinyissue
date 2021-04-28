3356fcc8f1f073a22ea213d4cc3a85b67c0bb808
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-04-28 19:20:45 -0400

    Titre du courriel contient des variables - 1

--- app/application/controllers/ajax/SendMail.php
+++ app/application/controllers/ajax/SendMail.php
@@ -79,7 +79,7 @@
 
 	if (Nombre($followers) > 0) {
 		while ($follower = Fetche($followers)) {
-			$subject = wildcards($subject, $follower,$ProjectID, $IssueID)
+			$subject = wildcards($subject, $follower,$ProjectID, $IssueID);
 			$passage_ligne = (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $follower["email"])) ? "\r\n" : "\n";
 			$message = str_replace('"', "``", $message);
 			$message = stripslashes($message);
